547bd28c1a4a19c53791928d59e009b58a13d8ef
Guru Nagajothi S <gurunagajothi2007@gmail.com>
2026-01-11 18:13:39 +0530

    Delete ai-crime-prediction directory

--- ai-crime-prediction/predict.py
+++
@@ -1,4 +0,0 @@
-def predict_risk(hour):
-    if hour >= 22 or hour <= 3:
-        return "HIGH RISK"
-    return "LOW RISK"
--- ai-crime-prediction/train_model.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from sklearn.linear_model import LinearRegression
-
-data = pd.read_csv("data/sample_crime_data.csv")
-X = data[["hour"]]
-y = data["crime_rate"]
-
-model = LinearRegression().fit(X, y)
-print("Model Trained")
